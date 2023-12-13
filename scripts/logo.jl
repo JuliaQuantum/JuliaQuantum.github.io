f41793be67ec0c8f076c64a45a59e74673d1fcfd
Roger-luo <rogerluo.rl18@gmail.com>
2023-12-13 15:40:43 -0500

    update logo

--- scripts/logo.jl
+++ scripts/logo.jl
@@ -1,5 +1,38 @@
 using Luxor
+using Colors
 
-Drawing(600, 400, "logo.svg")
-julialogo(action=:fill)
-preview()
+begin
+    Drawing(600, 400, "logo-dark.png")
+    origin()
+    # background("white")
+    translate(-20, 20)
+    scale(1.0)
+    juliacircles()
+    gsave()
+    sethue("white")
+    top = -190
+    bottom = 150
+    left = -230
+    right = 200
+    weight = 30
+    right_adjust = 3
+    poly([
+        Point(left, top), Point(left+weight, top),
+        Point(left+weight, bottom), Point(left, bottom),
+    ], action=:fill)
+    # a ⟩ mark at 200 using poly(<list>, ...) with flat ends
+    poly(
+        [
+            Point(right, top+right_adjust),
+            Point(right+weight, top+right_adjust),
+            Point(right+weight+50, (top+bottom)/2),
+            Point(right+weight, bottom-right_adjust),
+            Point(right, bottom-right_adjust),
+            Point(right+50, (top+bottom)/2),
+        ],
+        action = :fill
+    )
+    grestore()
+    finish()
+    preview()
+end
